dc4849157b3ce0cb7f963eb86fbc7c130b14cef1
phonghoac2502 <phong.hoacthieu@gmail.com>
2015-07-04 12:30:04 +0700

    asdas
    cascasc

--- Stormy/app/src/main/java/com/dat/stormy/location.java
+++ Stormy/app/src/main/java/com/dat/stormy/location.java
@@ -1,38 +1,140 @@
 package com.dat.stormy;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 
-public class location extends ActionBarActivity {
+public class location extends Activity {
+    final String TAG = location.class.getSimpleName();
+    EditText mLocation;
+    Button mButtonSearch;
+    String lon, lat;
+    final String GET_LONG_LAT = "http://maps.googleapis.com/maps/api/geocode/json?address=%1$s&sensor=true";
+    public final static String ADDRESS = "phonghoac";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+        mLocation = (EditText) findViewById(R.id.editText);
+        mButtonSearch = (Button) findViewById(R.id.btnSearch);
+        mButtonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String namePlace = mLocation.getText().toString();
+                if (namePlace.equals(""))
+                    Toast.makeText(location.this, "Please input name of place!", Toast.LENGTH_LONG).show();
+                else {
+                    final String key = String.format(GET_LONG_LAT, namePlace).replace(" ", "%20");
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            new LoadWebBody().execute(key);
+                        }
+                    });
+                }
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_location, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private String GetLongLatFromAddress(String json) {
+        String outPut = "";
+        try {
+            JSONObject body = new JSONObject(json);
+            JSONArray result = body.getJSONArray("results");
+            JSONObject temp = result.getJSONObject(0);
+            JSONObject geo = temp.getJSONObject("geometry");
+            JSONObject location = geo.getJSONObject("location");
+            double lat = location.getDouble("lat");
+            double lng = location.getDouble("lng");
+            outPut = "lat=" + lat + "&lon=" + lng;
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
+        Log.d(TAG, " GetLongLatFromAddress : " + outPut);
+        return outPut;
+    }
 
-        return super.onOptionsItemSelected(item);
+    class LoadWebBody extends AsyncTask<String, Integer, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            return getXmlFromUrl(params[0]);
+        }
+        protected void onPostExecute(String s) {
+            String address = GetLongLatFromAddress(s);
+            Intent intent = new Intent(location.this, MainActivity.class);
+            intent.putExtra(ADDRESS, address);
+            startActivity(intent);
+        }
+    }
+
+    private String getXmlFromUrl(String urlString) {
+        Log.d(TAG, " getXml : url " + urlString);
+        String responseBody = "Wrong";
+        if (isNetworkAvailable()) {
+            DefaultHttpClient http = new DefaultHttpClient();
+            HttpGet httpMethod = new HttpGet();
+            try {
+                httpMethod.setURI(new URI(urlString));
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+            HttpResponse response = null;
+            try {
+                response = http.execute(httpMethod);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            int responseCode = response.getStatusLine().getStatusCode();
+            switch (responseCode) {
+                case 200:
+                    HttpEntity entity = response.getEntity();
+                    if (entity != null) {
+                        try {
+                            responseBody = EntityUtils.toString(entity);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+            }
+        } else {
+            Toast.makeText(location.this, "Network is not available!", Toast.LENGTH_LONG).show();
+        }
+        Log.d(TAG, " getXmlFromUrl : " + responseBody);
+        return responseBody;
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+        return isAvailable;
     }
 }
